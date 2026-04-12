d49c2e81a0bc46c27201ef1090e69a26289d58c3
abmasud1214 <abmasud1214@gmail.com>
2026-04-12 14:27:16 -0700

    Fix server separation issues.

--- src/main/java/mod/journeycreative/keybinds/ModKeyBindings.java
+++ src/main/java/mod/journeycreative/keybinds/ModKeyBindings.java
@@ -4,13 +4,14 @@ import com.mojang.blaze3d.platform.InputConstants;
 import mod.journeycreative.JourneyCreative;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.Identifier;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.common.util.Lazy;
 import org.lwjgl.glfw.GLFW;
 
-@EventBusSubscriber(modid = JourneyCreative.MODID)
+@EventBusSubscriber(modid = JourneyCreative.MODID, value = Dist.CLIENT)
 public class ModKeyBindings {
     public static final KeyMapping.Category JOURNEY_CREATIVE = KeyMapping.Category.register(Identifier.fromNamespaceAndPath(JourneyCreative.MODID, "main"));
 
