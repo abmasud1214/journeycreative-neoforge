60299583dd32fbfab70e3fa4b1b0fa6c8bf8ae8d
abmasud1214 <abmasud1214@gmail.com>
2026-04-12 14:19:12 -0700

    Fix server separation issues.

--- src/main/java/mod/journeycreative/keybinds/KeyInputHandler.java
+++ src/main/java/mod/journeycreative/keybinds/KeyInputHandler.java
@@ -2,13 +2,14 @@ package mod.journeycreative.keybinds;
 
 import mod.journeycreative.JourneyCreative;
 import mod.journeycreative.networking.JourneyNetworking;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.network.ClientPacketDistributor;
 import net.neoforged.neoforge.network.PacketDistributor;
 
-@EventBusSubscriber(modid = JourneyCreative.MODID)
+@EventBusSubscriber(modid = JourneyCreative.MODID, value = Dist.CLIENT)
 public class KeyInputHandler {
     @SubscribeEvent
     public static void onClientTick(ClientTickEvent.Post event) {
